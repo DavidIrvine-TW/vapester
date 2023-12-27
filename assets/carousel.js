2657d1edb4bd4d1e6675223f890eda84d43231a0
David <david.irvineakamarv@gmail.com>
2023-12-27 15:46:32 +0700

    worked on the hero-banner

--- assets/carousel.js
+++ assets/carousel.js
@@ -6,3 +6,17 @@ $('.fade').slick({
     fade: true,
     cssEase: 'linear'
   });
+
+  $(document).ready(function() {
+    // Add click event listener to the button
+    $('#myButton').on('click', function() {
+      // Log a message to the console when the button is clicked
+      console.log('Button clicked!');
+    });
+  
+    // Add a passive scroll-blocking touchmove event listener
+    document.addEventListener('touchmove', function() {
+      // Your touchmove event handling code here
+    }, { passive: true });
+  });
+  
