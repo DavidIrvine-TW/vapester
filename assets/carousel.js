c1b7574465492c722191edcee51d5bc0c8832a7e
David Irvine <david.irvineakamarv@gmail.com>
2023-12-29 09:25:51 +0700

    adding svgs and selling-points setion

--- assets/carousel.js
+++ assets/carousel.js
@@ -4,12 +4,32 @@ $('.fade').slick({
     infinite: true,
     speed: 500,
     fade: true,
-    cssEase: 'linear'
+    // cssEase: 'linear'
   });
 
   $(document).ready(function() {
     $('.hamburger-menu').on('click', function() {
       $(this).toggleClass('cross');
     });
+  });
+
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const items = document.querySelectorAll('.single-item');
+  
+    function showItem(index) {
+      items.forEach(item => item.classList.remove('active'));
+      items[index].classList.add('active');
+    }
+  
+    function nextItem() {
+      const currentIndex = Array.from(items).findIndex(item => item.classList.contains('active'));
+      const nextIndex = (currentIndex + 1) % items.length;
+      showItem(nextIndex);
+    }
+  
+    // Change item every 3 seconds
+    setInterval(nextItem, 3000);
   });
   
