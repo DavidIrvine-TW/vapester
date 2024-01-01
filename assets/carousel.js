285b7f7031056b409eda8e0fe9e07715ef3d13eb
David <david.irvineakamarv@gmail.com>
2024-01-01 12:44:39 +0700

    completed a collection

--- assets/carousel.js
+++ assets/carousel.js
@@ -1,35 +1,35 @@
-// hero-banner
-// $('.fade').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     fade: true,
-//     // cssEase: 'linear'
-//   });
+hero-banner
+$('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
 
-  // $(document).ready(function() {
-  //   $('.hamburger-menu').on('click', function() {
-  //     $(this).toggleClass('cross');
-  //   });
-  // });
+  $(document).ready(function() {
+    $('.hamburger-menu').on('click', function() {
+      $(this).toggleClass('cross');
+    });
+  });
 
 
 
-  // document.addEventListener('DOMContentLoaded', function () {
-  //   const items = document.querySelectorAll('.single-item');
+  document.addEventListener('DOMContentLoaded', function () {
+    const items = document.querySelectorAll('.single-item');
   
-  //   function showItem(index) {
-  //     items.forEach(item => item.classList.remove('active'));
-  //     items[index].classList.add('active');
-  //   }
+    function showItem(index) {
+      items.forEach(item => item.classList.remove('active'));
+      items[index].classList.add('active');
+    }
   
-  //   function nextItem() {
-  //     const currentIndex = Array.from(items).findIndex(item => item.classList.contains('active'));
-  //     const nextIndex = (currentIndex + 1) % items.length;
-  //     showItem(nextIndex);
-  //   }
+    function nextItem() {
+      const currentIndex = Array.from(items).findIndex(item => item.classList.contains('active'));
+      const nextIndex = (currentIndex + 1) % items.length;
+      showItem(nextIndex);
+    }
   
-  //   // Change item every 3 seconds
-  //   setInterval(nextItem, 3000);
-  // });
+    // Change item every 3 seconds
+    setInterval(nextItem, 3000);
+  });
   
