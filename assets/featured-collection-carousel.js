af840b2f649f2b83dc5ad5ae4b858f5ac54fed45
David <david.irvineakamarv@gmail.com>
2024-01-01 13:47:55 +0700

    added featured-collection

---
+++ assets/featured-collection-carousel.js
@@ -0,0 +1,13 @@
+
+// $('.fade').slick({
+//     dots: false,
+//     infinite: true,
+//     speed: 500,
+//     fade: true,
+//     cssEase: 'linear'
+//   });
+
+$('.fade').slick();
+
+console.log('hello')
+
