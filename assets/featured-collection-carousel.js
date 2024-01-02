e6d5402c2246f305df95ae798341043348fe5552
David Irvine <david.irvineakamarv@gmail.com>
2024-01-02 07:49:43 +0700

    added sections

--- assets/featured-collection-carousel.js
+++ assets/featured-collection-carousel.js
@@ -1,13 +1,42 @@
-
-// $('.fade').slick({
-//     dots: false,
-//     infinite: true,
-//     speed: 500,
-//     fade: true,
-//     cssEase: 'linear'
-//   });
-
-$('.fade').slick();
-
-console.log('hello')
-
+$(".fade").slick({
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    
+    const fadeSlickInstance = $('.fade').slick('getSlick');
+    const numberOfFadeSlides = fadeSlickInstance.slideCount;
+    console.log('Number of slides for .fade carousel:', numberOfFadeSlides);
+  });
+  
