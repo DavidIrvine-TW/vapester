20865b2b218cd0f1dd9337ff4c08bdf705925587
David Irvine <david.irvineakamarv@gmail.com>
2024-01-04 07:14:07 +0700

    added media q to home page

--- assets/featured-collection-carousel.js
+++ assets/featured-collection-carousel.js
@@ -1,36 +1,10 @@
-$(".fade").slick({
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+$("#slick-hero").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true
   });
   
   document.addEventListener('DOMContentLoaded', function () {
